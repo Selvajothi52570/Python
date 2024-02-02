a6332ba2dd087b9195bf43c9dee5ef11501a59d0
Selvajothi52570 <144263942+Selvajothi52570@users.noreply.github.com>
2024-02-02 14:48:53 +0000

    Four commit

---
+++ Prg1.py
@@ -0,0 +1,23 @@
+#string and string function
+s="Selvajothi hai how are u"
+v="Muthukumar"
+"""print(s)
+print(s.upper())
+print(v.lower())
+print(s.capitalize())"""
+
+print(s.title())
+d="Selvajothi"
+print(d.count("l"))
+print(d.endswith("thi"))
+print(d.find("j",5))
+print(d.replace("a",'5'))
+
+q="7678687"
+print(q.isupper())
+print(q.islower())
+print(q.isalnum())
+print("hai\nis\ngood")
+
+i="Welcome selvajothi"
+print(len(i))
---
+++ Prg4.py
@@ -0,0 +1,4 @@
+side1=int(input("Enter a side"))
+side2=int(input("Enter a side"))
+side3=int(input("Enter a side"))
+if 
